f5906f66adce26e2954123c5851778130aabdc7b
Matheus Bortolucci <matheusbzo@hotmail.com>
2022-11-12 01:56:58 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -6,14 +6,14 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>THONK!</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="To batendo a nave..." />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          Curtindo a <code>solidão.js</code>
         </p>
       </main>
 
